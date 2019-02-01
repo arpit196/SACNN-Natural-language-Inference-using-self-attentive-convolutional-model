e1a30a759ad6097d7cb20f7536864c421042c80f
arpit196 <30366755+arpit196@users.noreply.github.com>
2019-02-01 21:04:23 +0530

    Update run.py

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from argparse import ArgumentParser
 import tensorflow as tf
 from tqdm import tqdm
 
-from data.dataset2 import Dataset, DATASETS
+from data.dataset import Dataset, DATASETS
 from models.model_type import MODELS
 from utils.batch_helper import BatchHelper
 from utils.config_helpers import MainConfig
@@ -158,7 +158,7 @@ def main():
                         help='pipeline mode')
 
     parser.add_argument('model',
-                        choices=['rnn', 'cnn', 'multihead','bcann','bcsann','bcsann1','twolayerbcnn','bcmultihead'],
+                        choices=['rnn', 'cnn', 'multihead','bcann','bcsann','bcsann1','twolayerbcnn','bcmultihead','bcsannlwr'],
                         help='model to be used')
 
     parser.add_argument('dataset',
