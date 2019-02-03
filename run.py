914d9832f8f2bb624c6e536cc234216f22985e67
arpit196 <30366755+arpit196@users.noreply.github.com>
2019-02-03 15:13:58 +0530

    Update run.py

--- run.py
+++ run.py
@@ -158,7 +158,7 @@ def main():
                         help='pipeline mode')
 
     parser.add_argument('model',
-                        choices=['rnn', 'cnn', 'multihead','bcann','bcsann','bcsann1','twolayerbcnn','bcmultihead','bcsannlwr'],
+                        choices=['rnn', 'cnn', 'multihead','bcann','bcsann','bcsann1','twolayerbcnn','bcmultihead','bcsannlwr','lstmmulti'],
                         help='model to be used')
 
     parser.add_argument('dataset',
