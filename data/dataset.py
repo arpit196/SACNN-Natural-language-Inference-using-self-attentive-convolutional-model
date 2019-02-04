26e02ed7d63ca3db361024f1138893df14b4b6a4
arpit196 <30366755+arpit196@users.noreply.github.com>
2019-02-04 18:04:03 +0530

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -6,7 +6,8 @@ import pandas as pd
 
 class DatasetType(Enum):
     SNLI = 0,
-    QQP = 1
+    QQP = 1,
+    MNLI = 2
 
 
 class ColumnType(Enum):
@@ -152,10 +153,57 @@ class SNLIDataset(DatasetExperiment):
     def _data_path(self):
         return 'corpora/SNLI/'
 
+class MNLIDataset(DatasetExperiment):
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        dataset = pd.read_csv('{}{}'.format(self.data_dir, 'train_snli.txt'),
+                              delimiter='\t', header=None, names=columns, na_values='')
+        dataset.dropna(inplace=True)
+        dataset = dataset.sample(frac=1, random_state=1).reset_index(drop=True)
+        num_instances = len(dataset)
+        self.num_train = num_instances * (1 - self.dev_ratio - self.test_ratio)
+        self.num_dev = num_instances * self.dev_ratio
+        self.num_test = num_instances * self.test_ratio
+        self.train = dataset.loc[:self.num_train]
+        self.dev = dataset.loc[self.num_train:self.num_train + self.num_dev]
+        self.test = dataset.loc[self.num_train + self.num_dev:self.num_train + self.num_dev + self.num_test]
+
+    def train_set(self):
+        return self.train
+
+    def train_set_pairs(self):
+        return self.train[[sentence1.name, sentence2.name]].as_matrix()
+
+    def train_labels(self):
+        return self.train[labels.name].as_matrix()
+
+    def dev_set(self):
+        return self.dev
+
+    def dev_set_pairs(self):
+        return self.dev[[sentence1.name, ColumnType.sentence2.name]].as_matrix()
+
+    def dev_labels(self):
+        return self.dev[ColumnType.labels.name].as_matrix()
+
+    def test_set(self):
+        return self.test
+
+    def test_set_pairs(self):
+        return self.test[[ColumnType.sentence1.name, ColumnType.sentence2.name]].as_matrix()
+
+    def test_labels(self):
+        return self.test[ColumnType.labels.name].as_matrix()
+
+    def _data_path(self):
+        return 'corpora/SNLI/'
+    
 
 DATASETS = {
     DatasetType.QQP.name: QQPDataset,
-    DatasetType.SNLI.name: SNLIDataset
+    DatasetType.SNLI.name: SNLIDataset,
+    DatasetType.MNLI.name: MNLIDataset
 }
 
 
